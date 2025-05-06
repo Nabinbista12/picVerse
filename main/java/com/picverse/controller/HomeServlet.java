f909b27623c01d7ecb8a5947b05e68990ac7a3f0
Nabin Bista <bistanabin98@gmail.com>
2025-05-06 13:09:42 +0545

    milestone 2

---
+++ main/java/com/picverse/controller/HomeServlet.java
@@ -0,0 +1,84 @@
+package com.picverse.controller;
+
+import com.picverse.config.DatabaseConfig;
+import com.picverse.model.PostModel;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Servlet implementation class HomeServlet
+ */
+@WebServlet({ "/", "/home" })
+public class HomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HomeServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+
+	// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+
+		ArrayList<PostModel> posts = new ArrayList<>();
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DatabaseConfig.getDbConnection();
+
+			String sql = "SELECT * FROM posts ORDER BY id DESC";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				PostModel post = new PostModel(rs.getInt("id"), rs.getString("caption"), rs.getString("image"));
+				posts.add(post);
+			}
+
+			rs.close();
+			stmt.close();
+			conn.close();
+
+		} catch (Exception e) {
+			e.printStackTrace(); // Log for now; later redirect to error page
+		}
+
+		request.setAttribute("posts", posts);
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/pages/home.jsp");
+		rd.forward(request, response);
+//		request.getRequestDispatcher("/WEB-INF/pages/home.jsp").forward(request, response);	
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
