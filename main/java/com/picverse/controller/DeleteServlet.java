ac4ab515d77a3d64871697297d9f26f574b6ede2
AnushkaAdhikari <adhikarianushka2020@gmail.com>
2025-05-06 14:50:17 +0545

    adding contact, amdin, usermanagement

---
+++ main/java/com/picverse/controller/DeleteServlet.java
@@ -0,0 +1,61 @@
+package com.picverse.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.picverse.config.DatabaseConfig;
+
+/**
+ * Servlet implementation class DeleteServlet
+ */
+@WebServlet("/delete")
+public class DeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+
+		try {
+			Connection conn = DatabaseConfig.getDbConnection();
+			String sql = "DELETE FROM posts WHERE id=?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, id);
+
+			stmt.executeUpdate();
+
+			stmt.close();
+			conn.close();
+
+			response.sendRedirect("home");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
